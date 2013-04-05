4fa1acf61cc1abee0501c11620a65061bae8d292
Jeff Tilson <jrtilson@gmail.com>
2013-04-05 10:14:22 -0400

    Updated descriptons

--- languages/en.php
+++ languages/en.php
@@ -12,21 +12,21 @@
 
 $english = array(
 	// Content strings
-	'spotseo:index' => 'Home!',
-	'spotseo:activity' => 'Activity!',
-	'spotseo:groups:all' => 'Groups!',
-	'spotseo:members' => 'Members!',
-	'spotseo:tagdashboards:all' => 'Tag Dashboards!',
-	'spotseo:blog:all' => 'Blogs!',
-	'spotseo:bookmarks:all' => 'Bookmarks!',
-	'spotseo:books:reading' => 'Books!',
-	'spotseo:file:all' => 'Files!',
-	'spotseo:pages:all' => 'Pages!',
-	'spotseo:photos:all' => 'Photos!',
-	'spotseo:rss' => 'RSS Feeds!',
-	'spotseo:rubrics' => 'Rubrics!',
-	'spotseo:videos' => 'Spot Videos!',
-	'spotseo:webvideos' => 'Web Videos!',
+	'spotseo:index' => "Spot is a mashup of a social network, classrooms, wikis, photos and more. Students and teachers meet in this virtual space to research, produce, publish and discuss.",
+	'spotseo:activity' => "What's happening on THINK Spot. All the latest blogs, bookmarks, photos discussions and more.",
+	'spotseo:groups:all' => "Browse the latest and most popular groups on THINK Spot. Groups are for classes, courses, clubs and those who share similar interests.",
+	'spotseo:members' => "THINK Spot members include faculty, students and staff. Browse the newest Spot users and see who's online.",
+	'spotseo:tagdashboards:all' => "Tag Dashboards on Spot are dynamic dashboards that display content with specific tags. Including blogs, photos, bookmarks and more.",
+	'spotseo:blog:all' => "Read blogs created by our students, staff and faculty as they document their travels throughout the world.",
+	'spotseo:bookmarks:all' => "A collection of bookmarks from the THINK Spot community. Useful sites, educational resources, and more.",
+	'spotseo:books:reading' => "See which books THINK Global School students, staff and faculty have been reading.",
+	'spotseo:file:all' => "File uploads on THINK Spot. Including: PDF's, documents, music, and more.",
+	'spotseo:pages:all' => "THINK Spot pages are collaborative wikis/documents created by students, faculty and staff.",
+	'spotseo:photos:all' => "View all the latest THINK Spot photos and albums submitted by students and faculty.",
+	'spotseo:rss' => "RSS Feeds submitted by the THINK Spot students, teachers and staff.",
+	'spotseo:rubrics' => "Collaborative rubrics created by THINK Spot faculty.",
+	'spotseo:videos' => "Videos uploaded by THINK Spot students, faculty and staff.",
+	'spotseo:webvideos' => "Web videos submitted by THINK Spot students, faculty and staff.",
 );
 
 add_translation('en',$english);
